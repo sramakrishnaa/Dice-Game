853e52963d7d3f7b169a70fe49af8e832413166d
Ramakrishna Sangamreddy <125732155+sramakrishnaa@users.noreply.github.com>
2023-07-07 20:52:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+var randomNumber2=Math.floor((Math.random()*6)+1);
+var randomNumber1=Math.floor((Math.random()*6)+1);
+
+if(randomNumber1===1){
+    document.querySelector(".img1").setAttribute("src","images/dice1.png");
+}
+else if(randomNumber1===2){
+    document.querySelector(".img1").setAttribute("src","images/dice2.png");
+}
+else if(randomNumber1===3){
+    document.querySelector(".img1").setAttribute("src","images/dice3.png");
+}
+else if(randomNumber1===4){
+    document.querySelector(".img1").setAttribute("src","images/dice4.png");
+}
+else if(randomNumber1===5){
+    document.querySelector(".img1").setAttribute("src","images/dice5.png");
+}
+
+
+
+
+if(randomNumber2===1){
+    document.querySelector(".img2").setAttribute("src","images/dice1.png");
+}
+else if(randomNumber2===2){
+    document.querySelector(".img2").setAttribute("src","images/dice2.png");
+}
+else if(randomNumber2===3){
+    document.querySelector(".img2").setAttribute("src","images/dice3.png");
+}
+else if(randomNumber2===4){
+    document.querySelector(".img2").setAttribute("src","images/dice4.png");
+}
+else if(randomNumber2===5){
+    document.querySelector(".img2").setAttribute("src","images/dice5.png");
+}
+
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="🚩 Player1 Win!";
+}
+else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML="Player2 Win! 🚩";
+}
+else{
+    document.querySelector("h1").innerHTML="Draw..!";
+}
